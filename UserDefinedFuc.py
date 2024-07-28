9302d3a9b194914c3c92eaa4e53ba1c696ee73cc
Atharv Kole <koleatharv2004@gmail.com>
2024-07-28 23:18:26 +0530

    Add files via upload

---
+++ UserDefinedFuc.py
@@ -0,0 +1,7 @@
+def calculate(a,b):
+    add=a+b
+    print(add) 
+
+c=5
+d=4
+calculate(c,d)    
---
+++ break.py
@@ -0,0 +1,7 @@
+for i in range(11):
+    
+    if(i==10):
+        break
+    print("5 x ", i+1,"=",5*(i+1)) 
+
+print("Done")
