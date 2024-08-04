d05a615dc4cbec34f767f96242b889d7d3ba112e
Atharv Kole <koleatharv2004@gmail.com>
2024-08-04 23:51:11 +0530

    Add files via upload

---
+++ set-problem.py
@@ -0,0 +1,21 @@
+set1={1,2,3,1,4,5}
+print(type(set1))
+print(set)
+
+s={"ak",22,123,"one"}
+print(s)
+
+#for empty set
+
+name={}
+print(type(name))
+
+app=set()
+print(type(app))
+
+
+#for set calling
+
+for value in s:
+    print(value)
+    
